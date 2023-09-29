6d529f3854e8c804fc5bcc8489c8894c5b6ed57e
Hvk <hoseakalayil@gmail.com>
2023-09-29 22:43:04 +0530

    added swagger documentastion

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{
     logger: ['error', 'warn', 'log', 'debug'],
   });
+
+  const options = new DocumentBuilder()
+    .setTitle('Algonaut API')
+    .setDescription('API for the algonaut application')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
